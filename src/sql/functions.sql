f31c2e14b02e07aa453a74efbec4f568746d5457
Kyle Magida <kyle@omelas.co>
2020-04-14 15:42:09 -0400

    add system tables

--- src/sql/functions.sql
+++ src/sql/functions.sql
@@ -1,7 +1,45 @@
-CREATE FUNCTION CDCMonthlyEnrollmentReporting (@ReportId int)
-    RETURNS TABLE
+CREATE OR ALTER FUNCTION CDCMonthlyEnrollmentReporting (@ReportId int)
+    RETURNS
+    @MonthlyEnrollmentsTemp TABLE (ChildId uniqueidentifier,
+    OrganizationId int,
+    OrganizationName nvarchar(100),
+    SiteId int,
+    SiteName nvarchar(100),
+    EnrollmentId int,
+    FamilyDeterminationId int,
+    FamilyId int,
+    ReportingPeriodId int,
+    ReportId int,
+    Period varchar(25),
+    ReportingPeriodStart date,
+    ReportingPeriodEnd date,
+    -- Used Variables
+    Sasid nvarchar(max),
+    LastName varchar(250),
+    FirstName varchar(250),
+    AgeGroupName varchar(50),
+    TimeName varchar(5),
+    SiteLicenseNumber int,
+    RegionName varchar(50),
+    TitleI bit,
+    Entry date,
+    [Exit] date,
+    NumberOfPeople int,
+    Income int,
+    Foster bit,
+    Accredited bit,
+    Rate decimal (18,2),
+    CDCRevenue decimal (18,2),
+    FundingSource int,
+    SMI75 int,
+    FPL200 int,
+    Under75SMI bit,
+    Under200FPL bit)
 AS
-    RETURN
+BEGIN
+DECLARE @SystemTime datetime;
+SET @SystemTime = (Select SubmittedAt FROM Report Where Id = @ReportID);
+INSERT INTO @MonthlyEnrollmentsTemp
 select
     Child.Id as ChildId,
     Organization.Id as OrganizationId,
@@ -41,20 +79,20 @@ select
          ELSE IncomeLevels.x200FPL
          END as FPL200,
     CASE WHEN Child.Foster = 1 THEN 1
-         WHEN FDTemp.Income < IncomeLevels.x75SMI THEN 1 
-         ELSE 0 
+         WHEN FDTemp.Income < IncomeLevels.x75SMI THEN 1
+         ELSE 0
          END as Under75SMI,
     CASE WHEN Child.Foster = 1 THEN 1
-         WHEN IncomeLevels.x200FPL >= FDTemp.Income THEN 1 
+         WHEN IncomeLevels.x200FPL >= FDTemp.Income THEN 1
          ELSE 0 END as Under200FPL
-    from Funding F
+    from Funding FOR SYSTEM_TIME AS OF @SystemTime AS F
     inner join ReportingPeriod RPCDC on
         F.FirstReportingPeriodId <= RPCDC.Id and (F.LastReportingPeriodId is null or F.LastReportingPeriodId >= RPCDC.Id)
-    inner join Enrollment on Enrollment.Id = EnrollmentId
+    inner join Enrollment FOR SYSTEM_TIME AS OF @SystemTime AS Enrollment on Enrollment.Id = EnrollmentId
     inner join Site on Site.Id = SiteId
     inner join Organization on Site.OrganizationId = Organization.Id
-    inner join Child on Child.Id = ChildId
-    inner join Family on Child.FamilyId = Family.Id
+    inner join Child FOR SYSTEM_TIME AS OF @SystemTime AS Child on Child.Id = ChildId
+    inner join Family FOR SYSTEM_TIME AS OF @SystemTime AS Family on Child.FamilyId = Family.Id
     INNER JOIN Report on Organization.Id = Report.OrganizationId and Report.ReportingPeriodId = RPCDC.Id
     LEFT OUTER JOIN Rates on Rates.RegionId = Site.Region and
                              Rates.Accredited = Report.Accredited and
@@ -73,13 +111,15 @@ select
             order by DeterminationDate desc
           ) as rn
 
-        from FamilyDetermination
+        from FamilyDetermination FOR SYSTEM_TIME AS OF @SystemTime as FamilyDetermination
       ) as FDTemp
       on FDTemp.FamilyId = Family.Id and rn = 1
     LEFT JOIN IncomeLevels on FDTemp.NumberOfPeople = IncomeLevels.NumberOfPeople
     WHERE F.Source = 0 and Report.Id = @ReportId;
+RETURN
+END
 
-CREATE FUNCTION CDCMonthlyOrganizationSpaceReporting(@ReportId int)
+CREATE OR ALTER FUNCTION CDCMonthlyOrganizationSpaceReporting(@ReportId int)
     RETURNS TABLE
 AS
     RETURN
@@ -114,9 +154,9 @@ SELECT
                                                  MER.AgeGroupName = NameLookup.AgeGroup and
                                                  MER.FundingSource = Report.Type
     WHERE Report.Id = @ReportId
-    GROUP BY RP.Id, Report.Id, RP.Period, RP.PeriodStart, RP.PeriodEnd, Report.Accredited, Report.Type, o.Id, o.Name, FS.Capacity, NameLookup.Time, NameLookup.AgeGroup;
+    GROUP BY RP.Id, Report.Id, RP.Period, RP.PeriodStart, RP.PeriodEnd, Report.Accredited, Report.Type, o.Id, o.Name, FS.Capacity, NameLookup.Time, NameLookup.AgeGroup
 
-CREATE FUNCTION CDCMonthlyOrganizationRevenueReporting(@ReportId int)
+CREATE OR ALTER FUNCTION CDCMonthlyOrganizationRevenueReporting(@ReportId int)
     RETURNS TABLE
 AS
     RETURN
@@ -138,5 +178,5 @@ SELECT
     FROM MonthlyOrganizationSpaceReporting MOSR
     INNER JOIN Report R on MOSR.ReportId = R.Id
     WHERE R.Id = @ReportId
-    GROUP BY MOSR.ReportingPeriodId, MOSR.ReportId, MOSR.Period, MOSR.ReportingPeriodStart, MOSR.ReportingPeriodEnd, MOSR.Accredited, MOSR.OrganizationId, MOSR.OrganizationName, R.FamilyFeesRevenue, R.C4KRevenue;
+    GROUP BY MOSR.ReportingPeriodId, MOSR.ReportId, MOSR.Period, MOSR.ReportingPeriodStart, MOSR.ReportingPeriodEnd, MOSR.Accredited, MOSR.OrganizationId, MOSR.OrganizationName, R.FamilyFeesRevenue, R.C4KRevenue, R.RetroactiveC4KRevenue
 
--- src/sql/temp_load.sql
+++ src/sql/temp_load.sql
@@ -1,8 +1,31 @@
-INSERT INTO MonthlyEnrollmentReporting
-select * from CDCMonthlyEnrollmentReporting('2019-11-01', '2020-04-03');
+-- Empty all tables
+DELETE FROM MonthlyEnrollmentReporting;
+DELETE FROM MonthlyOrganizationSpaceReporting;
+DELETE FROM MonthlyOrganizationRevenueReporting;
 
-INSERT INTO MonthlyOrganizationSpaceReporting
-SELECT * FROM CDCMonthlyOrganizationSpaceReporting('2019-11-01', '2020-04-03');
+-- Reload tables from all reports currently in the database
+DECLARE @MyCursor CURSOR;
+DECLARE @ReportId int;
+BEGIN
+    SET @MyCursor = CURSOR FOR
+    select Id from Report
 
-INSERT INTO MonthlyOrganizationRevenueReporting
-SELECT * FROM CDCMonthlyOrganizationRevenueReporting('2019-11-01', '2020-04-03');
+    OPEN @MyCursor
+    FETCH NEXT FROM @MyCursor
+    INTO @ReportId
+
+    WHILE @@FETCH_STATUS = 0
+    BEGIN
+        INSERT INTO MonthlyEnrollmentReporting
+        select * from CDCMonthlyEnrollmentReporting(@ReportId);
+        INSERT INTO MonthlyOrganizationSpaceReporting
+        SELECT * FROM CDCMonthlyOrganizationSpaceReporting(@ReportId);
+        INSERT INTO MonthlyOrganizationRevenueReporting
+        select * from CDCMonthlyOrganizationRevenueReporting(@ReportId);
+      FETCH NEXT FROM @MyCursor
+      INTO @ReportId
+    END;
+
+    CLOSE @MyCursor ;
+    DEALLOCATE @MyCursor;
+END;
