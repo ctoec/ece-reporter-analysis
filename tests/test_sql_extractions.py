57ffcda0f54d25d644b54f579d3c824687c433df
Kyle Magida <kyle@omelas.co>
2020-04-14 14:30:10 -0400

    set up testing classes

---
+++ tests/test_sql_extractions.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestSQLExtraction(unittest.TestCase):
+
+
+	def setUpClass(cls):
+		"""
+		Connect to database and set access to engine for all
+		:return:
+		"""
+
+
+	def test_foster_child(self):
+
+
+# Check valid foster logic
+
+
+
+if __name__ == '__main__':
+	unittest.main()
