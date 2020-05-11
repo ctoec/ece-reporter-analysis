47e2d450578753c635d32f74e9eb257a62e28c12
Kyle Magida <kyle@omelas.co>
2020-05-11 11:40:39 -0400

    add back in dummy table

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-set -e
-
 # Wait for DB to come up
+set -e
 while ! /opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'select 1';
 do
 echo "waiting"
 sleep 10
 done
 
-# Run tests and write results to volumes
+# Add dummy table to test against
+/opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'CREATE TABLE new_table (a int, b int)';
+
+/opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'INSERT INTO new_table VALUES (1,2), (2,3)';
+
+# Run tests
 pytest tests/ --junitxml=tests/test-results.xml
