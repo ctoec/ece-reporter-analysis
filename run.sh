bbb5cc5ad30a0e29e94e97efd51460fe7d5b9fc5
Kyle Magida <kyle@omelas.co>
2020-05-11 11:35:06 -0400

    clean up

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# Wait for DB to come up
 set -e
+
+# Wait for DB to come up
 while ! /opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'select 1';
 do
 echo "waiting"
 sleep 10
 done
-/opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'CREATE TABLE new_table (a int, b int)';
 
-/opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'INSERT INTO new_table VALUES (1,2), (2,3)';
-
+# Run tests and write results to volumes
 pytest tests/ --junitxml=tests/test-results.xml
