3863d7af130b7d5d50aa8f1af6d90579e63b79e6
Kyle Magida <kyle@omelas.co>
2020-05-11 11:20:36 -0400

    change source directory

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ done
 
 /opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'INSERT INTO new_table VALUES (1,2), (2,3)';
 
-pytest tests/ --junitxml=junit/test-results.xml
+pytest tests/ --junitxml=tests/test-results.xml
