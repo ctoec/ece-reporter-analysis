5fba88988f42d1a0c9214aa6fdeeedc8c70b2773
Kyle Magida <kyle@omelas.co>
2020-05-12 10:54:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ done
 /opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'CREATE TABLE new_table (a int, b int, c int)';
 /opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'INSERT INTO new_table VALUES (1,2, 4), (2,3, 4)';
 
-# Run tests and write results to test volume
+# Run tests and write results to test volume for printing
 pytest tests/ --junitxml=tests/test-results.xml
