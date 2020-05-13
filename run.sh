bd8b273ea9e116909760fc90e175a49b3b3db7ef
Kyle Magida <kmagida110@users.noreply.github.com>
2020-05-13 16:30:35 -0400

    Azure pipeline integration (#7)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * add python to pipeline
    
    * yml bug #1
    
    * yml edit 2
    
    * test publishing tests
    
    * actually add test in
    
    * test PR
    
    * add docker compose
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * move docker compose
    
    * adjust indentation
    
    * change yaml to yml
    
    * Update azure-pipelines.yml
    
    * remove excess tasks
    
    * change source directory
    
    * clean up
    
    * add back in dummy table
    
    * add documentation
    
    * add more test
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * automated test
    
    Co-authored-by: Cailyn Hansen <cailyn.e.hansen+github@gmail.com>

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Wait for DB to come up to continue with tests
+set -e
+while ! /opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'select 1';
+do
+echo "waiting for DB"
+sleep 10
+done
+
+# Add dummy table to run python test against
+/opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'CREATE TABLE new_table (a int, b int, c int)';
+/opt/mssql-tools/bin/sqlcmd -S test_db -U sa -P TestPassword1 -q 'INSERT INTO new_table VALUES (1,2, 4), (2,3, 4)';
+
+# Run tests and write results to test volume for printing
+pytest tests/ --junitxml=tests/test-results.xml
---
+++ tests/test_sql_extractions.py
@@ -0,0 +1,33 @@
+import unittest
+import sqlalchemy
+import time
+import pandas as pd
+
+FT = 'FT'
+PT = 'PT'
+INFANT = 'Infant/Toddler'
+PRESCHOOL = 'Preschool'
+SCHOOL_AGE = 'School-age'
+
+class TestSQLExtraction(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		"""
+		Connect to database and set access to engine for all, runs before all other tests
+		"""
+		conn_string = f"mssql+pyodbc://sa:TestPassword1@test_db/master?driver=ODBC+Driver+17+for+SQL+Server&Mars_Connection=Yes"
+		engine = sqlalchemy.create_engine(conn_string)
+
+		# Build in a wait to check for db coming up
+		cls.conn = engine.connect()
+
+	def test_connection(self):
+		self.assertEqual(1, 1)
+
+		test = self.conn.execute('select * from new_table').fetchall()
+		print(test)
+
+
+if __name__ == '__main__':
+	unittest.main()
